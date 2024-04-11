9947e3fa308ab516a20957c1e8d3075e710d1fe6
unknown <yuzhenkovsan@gmail.com>
2024-04-11 09:15:46 +0300

    adding clss work + renaming dir2

---
+++ pythonProject/3/ClassWork.3.py
@@ -0,0 +1,92 @@
+# code reading, сверху вниз
+
+#print(i)
+#i = 1 #name 'i' is not defined. Did you mean: 'id'?
+
+#========================================================#
+
+#coments
+#print(i)
+#i = 1 #name 'i' is not defined. Did you mean: 'id'?
+
+#========================================================#
+
+# naming
+
+#i =1
+#my_variable = 2
+#etc = 34
+
+#========================================================#
+
+# create a variable
+
+#new_my_variable = 44
+
+#========================================================#
+
+# change variable
+
+my_variable = 2
+
+my_variable += 44
+
+my_variable -= 44
+
+my_variable **= 2
+
+print(my_variable)
+
+#========================================================#
+
+# operators
+
+#number = 2
+#print(number + 2)
+#print(number - 2)
+#print(number * 2)
+#print(number / 2)
+#print(number // 2)
+#print(number % 2)
+#print(number ** 2)
+
+#number += 2
+#number -= 2
+#number *= 2
+#number /= 2
+#number //= 2
+#number %= 2
+#number **= 2
+#print(number)
+#========================================================#
+
+# type casting
+
+num = 1 # - все, что не ноль это True
+print(bool(num))
+
+num_2 = 0 # - ноль - всегда False
+print(bool(num_2))
+
+string = 'test' #- текст это тоже один - True
+print(bool(string))
+
+string_2 = '' # текст - остутсвие текста - ноль False
+print(bool(string_2))
+
+#========================================================#
+
+# type() function
+
+n = 1
+print(n)
+print(type(n))
+
+n = 2.0
+print(type(n))
+
+n = '2.0'
+print(type(n))
+
+
+
